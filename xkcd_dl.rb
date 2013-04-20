d63769677258f0fb2f84b55646526b3d6d162a45
Nathan Wallace <nathan.m.wallace@gmail.com>
2013-04-20 16:34:39 -0400

    extract out Comic class

--- xkcd_dl.rb
+++ xkcd_dl.rb
@@ -1,65 +1,68 @@
+require 'rubygems'
 require 'rss'
 require 'open-uri'
-require 'rmagick'
+require 'RMagick'
 require 'cgi'
 
-def download_image(item, path)
-  desc = item.description
-  src = get_attr("src", desc)
-  title = get_attr("title", desc)
-  file_path = "#{path}/#{item.title}.png"
-  File.open(file_path, "wb") do |f|
-    f.write open(src).read
+class Comic
+  def initialize(url, file_path)
+    @file_path = file_path
+    File.open(file_path, "wb") do |f|
+      f.write open(url).read
+    end
+    @img = Magick::ImageList.new(file_path)
   end
-  annotate_img(file_path, item.title, title)
-end
 
-def get_attr(attr, element)
-  CGI.unescapeHTML(element[/#{attr}="([^"]*)"/, 1])
-end
-
-def annotate_img(path, title, desc)
-  img = Magick::ImageList.new(path)
-  size_to_screen(img)
-  add_text(img, title, desc)
-  img.append(true).write(path)
-end
-
-def size_to_screen(img)
-  resolution = `xdpyinfo | grep 'dimensions:' | awk '{print $2}'`
-  width = resolution[/\d+[^x]/].to_i
-  height = resolution[/x(\d+)/, 1].to_i
-  img.border!((width-img.columns)/2, 50, "black")
-end
-
-def add_text(img, title, desc)
-  txt = Magick::Draw.new
-  img.annotate(txt,0,0,0,0,title) do
-    txt.gravity = Magick::NorthGravity
-    txt.font = "Helvetica Neue"
-    txt.pointsize = 32
-    txt.fill = "#FFFFFF"
-    txt.font_weight = Magick::BoldWeight
+  def size_to(width=1280, height=720)
+    @img.border!((width-@img.columns)/2, 50, "black")
   end
-  img << Magick::Image.read("caption:#{desc}") do
-    self.size = "#{img.first.columns}"
-    self.font = "Helvetica Neue"
-    self.pointsize = 16
-    self.fill = "#FFFFFF"
-    self.gravity = Magick::SouthGravity
-    self.background_color = "#000000"
-  end.first
+
+  def annotate(title, hover_text)
+    txt = Magick::Draw.new
+    @img.annotate(txt,0,0,0,0,title) do
+      txt.gravity = Magick::NorthGravity
+      txt.font = "Helvetica Neue"
+      txt.pointsize = 32
+      txt.fill = "#FFFFFF"
+      txt.font_weight = Magick::BoldWeight
+    end
+    size = @img.first.columns
+    @img << Magick::Image.read("caption:#{hover_text}") do
+      self.size = "#{size}"
+      self.font = "Helvetica Neue"
+      self.pointsize = 16
+      self.fill = "#FFFFFF"
+      self.gravity = Magick::SouthGravity
+      self.background_color = "#000000"
+    end.first
+  end
+
+  def save
+    @img.append(true).write(@file_path)
+  end
+end
+
+def parse_attr(attr, text)
+  CGI.unescapeHTML(text[/#{attr}="([^"]*)"/, 1])
 end
 
 url = "http://xkcd.com/rss.xml"
-img_dir = "/Users/Nathan/Desktop/xkcd"
+img_dir = "/home/nathan/Images/xkcd"
 
 open(url) do |rss|
   feed = RSS::Parser.parse(rss)
   puts "Downloading recent comics..."
   feed.items.each do |item|
-    download_image(item, img_dir)
-    puts "  #{item.title}"
+    print "  #{item.title}"
+    url = parse_attr("src", item.description)
+    comic = Comic.new(url, "#{img_dir}/#{item.title}.png")
+    print "."
+    comic.size_to
+    print "."
+    comic.annotate(item.title, parse_attr("title", item.description))
+    print "."
+    comic.save
+    puts "done"
   end
   puts "Done!"
 end
